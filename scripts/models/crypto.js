429a33195ec0e007118cdc8d4f10fb35a94f65e1
Mrebb <madhu.rebbana@gmail.com>
2018-04-28 14:30:49 -0700

    crypto constructor, main-view search calls to server

--- scripts/models/crypto.js
+++ scripts/models/crypto.js
@@ -13,11 +13,22 @@ ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
   function Crypto(marketDataObj){
     Object.keys(marketDataObj).foreach(key => this[key] = marketDataObj[key])
   }
+  Crypto.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#coin-results-template').text());
+    return template(this);
+  }
+  Crypto.all = [];
+  Crypto.loadAll = rows => Crypto.all = rows.map(Crypto => new Crypto(Crypto));
+  Crypto.fetchAll = (name) =>
+    $.getJSON(`${ENV.apiUrl}/api/v1/coins/${name}`)
+      .then(Crypto.loadAll)
+
+
   Crypto.one = [];
   Crypto.fetchCoin = (name) =>
     $.getJSON(`${ENV.apiUrl}/api/v1/coins/${name}`)
       .then(results => Crypto.one = results.map(coin => new Crypto(coin)))
-
+  
   module.Crypto = Crypto;
 
 })(app);

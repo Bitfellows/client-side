17c7e52cb56ff9393d14b6ae17c861797c826f00
Mrebb <madhu.rebbana@gmail.com>
2018-04-27 22:50:15 -0700

    ENV API URL for switching between local, remote db

--- scripts/models/crypto.js
+++ scripts/models/crypto.js
@@ -1,1 +1,9 @@
 'use strict';
+var app = app || {};
+
+const ENV = {};
+
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'https://bitfellows.herokuapp.com';
+ENV.developmentApiUrl = 'http://localhost:3000';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;

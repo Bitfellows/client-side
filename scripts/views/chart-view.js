213cbd11fb81f148842f4d51360e2536b2867bf1
James McDaniel <icathaid@gmail.com>
2018-04-30 19:23:52 -0700

    pushing chart 1 code

--- scripts/views/chart-view.js
+++ scripts/views/chart-view.js
@@ -1,37 +1,42 @@
 'use strict';
-// placeholder variables  c* indicates graph colors, these will later be dictated by CSS
-//  price* will later be populated by API data from server.js
-var c1 = 'blue';
-var c2 = 'red';
-var c3 = 'yellow';
-var c4 = 'green';
-var price1 = 3000;
-var price2 = 1244;
-var price3 = 646;
-var price4 = 2232;
-var price5 = 1877;
-var change1 = 0.05;
-var change2 = 0.125;
-var varCoin1 = 'BTC';
+//  placeholder variables, replace with data coming from API/CSS
+//  change variables will need a function to get current prices and compare them to their 7d weighted averages
 
-var labels = ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4', 'placeholder 5'];
-//  last value must be an extra 0 as work-around for chartJS issue.
-var price = [price1, price2, price3, price4, price5, 0];
-var priceData = {
-  labels: labels,
-  datasets: [{
-    data: price,
-    label: 'Price',
-    
-    backgroundColor: [c1, c2, c1, c2, c1],
-  }]
+var c1 = 'red'; 
+var c2 = 'blue';
+var c3 = 'green';
+var c4 = 'yellow';
+var val1a = 50;
+var val1b = 50;
+var val2a = 60;
+var val2b = 55;
+var val3a = 40;
+var val3b = 35;
+var val4a = 25;
+var val4b = 67;
+
+var barChartData = {
+  labels: ["Coin 1", "Coin 2", "Coin 3", "Coin 4"],
+  datasets: [
+    {
+      label: "Current Price",
+      backgroundColor: c1,
+      data: [val1a, val2a, val3a, val4a, 0]  
+    },
+    {
+      label: "7d Weighted Avg",
+      backgroundColor: c2,
+      data: [val1b, val2b, val3b, val4b]
+    },
+  ]
 };
 
+
 function makeChart (){
-  var ctx = $("#priceChart");
+  var ctx = $("#testerooChart");
   var priceChart = new Chart(ctx, {
     type: 'bar',
-    data: priceData,
+    data: barChartData,
     options: {
       responsive: false,
       scales: {
@@ -43,51 +48,4 @@ function makeChart (){
     },
   })
 };
-// makeChart();
-
-function testChart (){
-  var ctx = $("#testChart");
-  var comparePrice1 = price1 * change1;
-  var comparePrice2 = price2 * change2;
-  var newTestChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Price:USD'],
-      datasets: [
-        {
-          Label: 'Low',
-          backgroundColor: c1,
-          stack: 0,
-          data: [price1]
-        },
-        {
-          Label: 'High',
-          backgroundColor: c2,
-          stack: 1,
-          data: [comparePrice1],
-          
-        },
-        {
-          Label: 'Low', 
-          backgroundColor: c3,
-          stack: 2,
-          data: [price2]
-        },
-        {
-          Label: 'High',
-          backgroundColor: c4,
-          stack: 3,
-          data: [comparePrice2]
-        }
-      ],
-    },
-    options: {
-      scales: {
-        xAxes: [{stacked: true}],
-        yAxes: [{stacked: true}]
-      }
-    }
-  })
-};
-// testChart();
-
+makeChart();

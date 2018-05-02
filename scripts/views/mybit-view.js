7ed8ac2afad067485d9bef76111296fec34c8b08
Laboni A <asthanal@yahoo.com>
2018-05-01 21:48:32 -0700

    Worked on building mybit page and mybitview

--- scripts/views/mybit-view.js
+++ scripts/views/mybit-view.js
@@ -7,8 +7,34 @@ var app = app || {};
   const mybitView = {};
 
   mybitView.init = () => {
+    $('#login').hide();
+    $('#create-user').hide();
+    $('#overView').hide();
+    // $('#navHeader').show();
+    $('.main-header').show();
     $('#mybitView').show();
+    $('#update-form').hide();
+    $('#mybitView').show();
+    $("#clear-activity").hide();
+
   }
+  $("#update").click(function () {
+    $("#update-form").show();
+  }); 
+
+  $("#clear").click(function () {
+    $("#clear-activity").show();
+  });
+
+  $(".close").click(function(){
+    $("#clear-activity").hide();
+  });  
+  
+  $(".close").click(function(){
+    $("#update-form").hide();
+  });
+  
   module.mybitView = mybitView;
 
 })(app);
+
